98a77959e97e94641c2aa11657cb206a4d334834
darkfriend <maslov.tlt@gmail.com>
2019-08-06 21:24:38 +0300

    codestyle fix

--- audioPlayerMini.vue
+++ audioPlayerMini.vue
@@ -1,132 +1,139 @@
 <template>
-	<div class="player">
-		<slot name="play" :data="playing">
-			<a @click.prevent="playing = !playing" title="Play/Pause" href="#">
-				<svg width="18px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-					<path v-if="!playing" fill="currentColor" d="M15,10.001c0,0.299-0.305,0.514-0.305,0.514l-8.561,5.303C5.51,16.227,5,15.924,5,15.149V4.852c0-0.777,0.51-1.078,1.135-0.67l8.561,5.305C14.695,9.487,15,9.702,15,10.001z"/>
-					<path v-else fill="currentColor" d="M15,3h-2c-0.553,0-1,0.048-1,0.6v12.8c0,0.552,0.447,0.6,1,0.6h2c0.553,0,1-0.048,1-0.6V3.6C16,3.048,15.553,3,15,3z M7,3H5C4.447,3,4,3.048,4,3.6v12.8C4,16.952,4.447,17,5,17h2c0.553,0,1-0.048,1-0.6V3.6C8,3.048,7.553,3,7,3z"/>
-				</svg>
-			</a>
-		</slot>
-		<audio
-			:loop="innerLoop"
-			ref="audiofile"
-			:src="file"
-			preload="auto"
-			style="display: none;"
-		></audio>
-	</div>
+    <div class="player">
+        <slot name="play" :data="playing">
+            <a @click.prevent="playing = !playing" title="Play/Pause" href="#">
+                <svg width="18px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <path v-if="!playing" fill="currentColor"
+                          d="M15,10.001c0,0.299-0.305,0.514-0.305,0.514l-8.561,5.303C5.51,16.227,5,15.924,5,15.149V4.852c0-0.777,0.51-1.078,1.135-0.67l8.561,5.305C14.695,9.487,15,9.702,15,10.001z"/>
+                    <path v-else fill="currentColor"
+                          d="M15,3h-2c-0.553,0-1,0.048-1,0.6v12.8c0,0.552,0.447,0.6,1,0.6h2c0.553,0,1-0.048,1-0.6V3.6C16,3.048,15.553,3,15,3z M7,3H5C4.447,3,4,3.048,4,3.6v12.8C4,16.952,4.447,17,5,17h2c0.553,0,1-0.048,1-0.6V3.6C8,3.048,7.553,3,7,3z"/>
+                </svg>
+            </a>
+        </slot>
+        <audio
+            :loop="innerLoop"
+            ref="audiofile"
+            :src="file"
+            preload="auto"
+            style="display: none;"
+        ></audio>
+    </div>
 </template>
 
 <script>
-	import {empty} from './empty';
-	export default {
-		name: "audioPlayerMini",
-		props: {
-			file: String,
-			loop: {
-				type: Boolean,
-				default: false,
-			},
-			autoPlay: {
-				type: Boolean,
-				default: false
-			},
-		},
-		data() {
-			return {
-				innerLoop: false,
-				audio: undefined,
-				currentSeconds: 0,
-				durationSeconds: 0,
-				loaded: false,
-				playing: false,
-				previousVolume: 35,
-				showVolume: false,
-				volume: 100
-			};
-		},
-		created() {
-			this.innerLoop = this.loop;
-		},
-		mounted() {
-			this.audio = this.$el.querySelectorAll('audio')[0];
-			this.audio.addEventListener('timeupdate', this.update);
-			this.audio.addEventListener('loadeddata', this.load);
-			this.audio.addEventListener('pause', () => {
-				if(this.playing)
-					this.playing = false;
-			});
-			this.audio.addEventListener('play', () => { this.playing = true; });
-			this.audio.addEventListener('ended', () => { this.playing = false; });
-		},
-		computed: {
-			currentTime() {
-				return this.convertTimeHHMMSS(this.currentSeconds);
-			},
-			durationTime() {
-				return this.convertTimeHHMMSS(this.durationSeconds);
-			},
-			percentComplete() {
-				return parseInt(this.currentSeconds / this.durationSeconds * 100);
-			},
-			muted() {
-				return this.volume / 100 === 0;
-			}
-		},
-		watch: {
-			playing(value) {
-				if (value) {
-					if(!empty(window.darkPlayer)) {
-						window.darkPlayer.stop();
-					}
-					window.darkPlayer = this.audio;
-					return this.audio.play();
-				} else {
-					window.darkPlayer = false;
-				}
-				this.audio.pause();
-			},
-			volume(value) {
-				this.showVolume = false;
-				this.audio.volume = this.volume / 100;
-			}
-		},
-		methods: {
-			convertTimeHHMMSS(val){
-				let hhmmss = new Date(val * 1000).toISOString().substr(11, 8);
-				return hhmmss.indexOf("00:") === 0 ? hhmmss.substr(3) : hhmmss;
-			},
-			load() {
-				if (this.audio.readyState >= 2) {
-					this.loaded = true;
-					this.durationSeconds = parseInt(this.audio.duration);
-					return this.playing = this.autoPlay;
-				}
-				throw new Error('Failed to load sound file.');
-			},
-			mute() {
-				if (this.muted) {
-					return this.volume = this.previousVolume;
-				}
-				this.previousVolume = this.volume;
-				this.volume = 0;
-			},
-			seek(e) {
-				if (!this.playing || e.target.tagName === 'SPAN') return;
-				const el = e.target.getBoundingClientRect();
-				const seekPos = (e.clientX - el.left) / el.width;
-				this.audio.currentTime = parseInt(this.audio.duration * seekPos);
-			},
-			stop() {
-				this.playing = false;
-				this.audio.currentTime = 0;
-			},
-			update(e) {
-				this.currentSeconds = parseInt(this.audio.currentTime);
-			}
-		},
-	}
+  import {empty} from './empty';
+
+  export default {
+    name: "audioPlayerMini",
+    props: {
+      file: String,
+      loop: {
+        type: Boolean,
+        default: false,
+      },
+      autoPlay: {
+        type: Boolean,
+        default: false
+      },
+    },
+    data() {
+      return {
+        innerLoop: false,
+        audio: undefined,
+        currentSeconds: 0,
+        durationSeconds: 0,
+        loaded: false,
+        playing: false,
+        previousVolume: 35,
+        showVolume: false,
+        volume: 100
+      };
+    },
+    created() {
+      this.innerLoop = this.loop;
+    },
+    mounted() {
+      this.audio = this.$el.querySelectorAll('audio')[0];
+      this.audio.addEventListener('timeupdate', this.update);
+      this.audio.addEventListener('loadeddata', this.load);
+      this.audio.addEventListener('pause', () => {
+        if (this.playing)
+          this.playing = false;
+      });
+      this.audio.addEventListener('play', () => {
+        this.playing = true;
+      });
+      this.audio.addEventListener('ended', () => {
+        this.playing = false;
+      });
+    },
+    computed: {
+      currentTime() {
+        return this.convertTimeHHMMSS(this.currentSeconds);
+      },
+      durationTime() {
+        return this.convertTimeHHMMSS(this.durationSeconds);
+      },
+      percentComplete() {
+        return parseInt(this.currentSeconds / this.durationSeconds * 100);
+      },
+      muted() {
+        return this.volume / 100 === 0;
+      }
+    },
+    watch: {
+      playing(value) {
+        if (value) {
+          if (!empty(window.darkPlayer)) {
+            window.darkPlayer.stop();
+          }
+          window.darkPlayer = this.audio;
+          return this.audio.play();
+        } else {
+          window.darkPlayer = false;
+        }
+        this.audio.pause();
+      },
+      volume(value) {
+        this.showVolume = false;
+        this.audio.volume = this.volume / 100;
+      }
+    },
+    methods: {
+      convertTimeHHMMSS(val) {
+        let hhmmss = new Date(val * 1000).toISOString().substr(11, 8);
+        return hhmmss.indexOf("00:") === 0 ? hhmmss.substr(3) : hhmmss;
+      },
+      load() {
+        if (this.audio.readyState >= 2) {
+          this.loaded = true;
+          this.durationSeconds = parseInt(this.audio.duration);
+          return this.playing = this.autoPlay;
+        }
+        throw new Error('Failed to load sound file.');
+      },
+      mute() {
+        if (this.muted) {
+          return this.volume = this.previousVolume;
+        }
+        this.previousVolume = this.volume;
+        this.volume = 0;
+      },
+      seek(e) {
+        if (!this.playing || e.target.tagName === 'SPAN') return;
+        const el = e.target.getBoundingClientRect();
+        const seekPos = (e.clientX - el.left) / el.width;
+        this.audio.currentTime = parseInt(this.audio.duration * seekPos);
+      },
+      stop() {
+        this.playing = false;
+        this.audio.currentTime = 0;
+      },
+      update(e) {
+        this.currentSeconds = parseInt(this.audio.currentTime);
+      }
+    },
+  }
 </script>
 
 <style scoped>
